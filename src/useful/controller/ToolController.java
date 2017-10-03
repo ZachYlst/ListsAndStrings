8a6dd1f0a5e8836742a4098af7b41e76418e8e74
ZachYlst <ZachYlst@users.noreply.github.com>
2017-10-03 08:59:43 -0600

    Added donutList + comments to explain code.

--- src/useful/controller/ToolController.java
+++ src/useful/controller/ToolController.java
@@ -2,12 +2,20 @@ package useful.controller;
 
 import java.util.List;
 import java.util.ArrayList;
+import useful.model.Donut;
 
 public class ToolController
 {
-
+	private List<Donut> donutList;
+	
+	public ToolController()
+	{
+		donutList = new ArrayList<Donut>();		// () calls constructor of ArrayList
+	}
+	
 	public void start()
 	{
-		
+		Donut temp = new Donut();
+		donutList.add(temp);
 	}
 }
