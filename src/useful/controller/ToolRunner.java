cb5ae79c1e7e28f2425732a14197945657307c95
ZachYlst <ZachYlst@users.noreply.github.com>
2017-10-03 08:44:04 -0600

    Added in a donutClass + data members.

--- src/useful/controller/ToolRunner.java
+++ src/useful/controller/ToolRunner.java
@@ -1,6 +1,16 @@
 package useful.controller;
 
+/**
+ * 
+ * @author zyls7153
+ *
+ */
+
 public class ToolRunner
 {
-
+	public static void main(String [] args)
+	{
+		ToolController myApp = new ToolController();
+		myApp.start();		// () followed by a ; indicates a call
+	}
 }
---
+++ src/useful/model/Donut.java
@@ -0,0 +1,12 @@
+package useful.model;
+
+public class Donut
+{
+	private boolean hasSprinkles;
+	private boolean hasGlaze;
+	private String shape;
+	private String flavor;
+	private int holeCount;
+	
+	
+}
